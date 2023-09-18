d1d5b9051a3f5cf5d76a48ce88ea2678e00f4e7c
shubhitech <shubhi171197gupta@gmail.com>
2023-09-18 22:24:33 +0530

    First Commit

---
+++ NewGit/src/in/main/pixcel/New.java
@@ -0,0 +1,17 @@
+package in.main.pixcel;
+
+public class New {
+
+	public static void main(String[] args) {
+    int [] arr1 = {10,20,30};
+    int [] arr2 = {10,20,30};
+    
+    if(arr1==arr2) {
+    	System.out.println("true");
+    }else
+    {
+    	System.out.println("false");
+    }
+	}
+
+}
